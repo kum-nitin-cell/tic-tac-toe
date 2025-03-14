d0038e12fa2140440499a212a682e2a5bd084202
kum-nitin-cell <nitin.kumar2@ucalgary.ca>
2025-03-14 17:11:40 -0600

    final updated

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function Square({ value, onSquareClick }) {
 
 function findWinner(board) {
   const winningPatterns = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-    [0, 4, 8], [2, 4, 6]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
   ];
 
   for (let [a, b, c] of winningPatterns) {
